93d6ea875f9fcc1d3f961110c5968356427b4f4d
balaxon <balahanov2209@gmail.com>
2020-03-21 23:32:37 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(arguments)
+
+  for (let i=0; i < bracketsConfig.length;){
+    if (str.includes(bracketsConfig[i].join(''))){
+      str = str.replace(bracketsConfig[i].join(''),'');
+      i=0;
+    }
+    else{i=i+1;}
+  }
+  if(str.length){return false;}
+  return true;
 }
